44f9e23f93fa18d1b3958d0fe72cfe2120b4434b
Alex Tatum <tatum_alex@ymail.com>
2020-12-17 14:24:20 -0500

    fixed connection

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ if(process.env.JAWSDB_URL) {
 } else {
   connection = mysql.createConnection({
     host: "localhost",
-      port: 3000,
+      port: 3306,
       user: "root",
       password: "Jeffmx480",
       database: "burgers_db",
